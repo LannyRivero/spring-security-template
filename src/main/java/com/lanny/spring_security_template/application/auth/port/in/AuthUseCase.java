75aff91938b660758a0678e11536f3a0923721dd
lannyrivero <lannyrivero288@gmail.com>
2025-11-04 13:56:56 +0100

    fix: adjust indentation in AuthUseCase interface

--- src/main/java/com/lanny/spring_security_template/application/auth/port/in/AuthUseCase.java
+++ src/main/java/com/lanny/spring_security_template/application/auth/port/in/AuthUseCase.java
@@ -1,5 +1,5 @@
 package com.lanny.spring_security_template.application.auth.port.in;
 
 public interface AuthUseCase {
-    // Métodos se definirán en F2 (login, refresh, me)
+  // Métodos se definirán en F2 (login, refresh, me)
 }
