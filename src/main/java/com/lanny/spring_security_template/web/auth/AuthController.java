4925b8d883e3812063d10fca812cef2924b6202f
lannyrivero <lannyrivero288@gmail.com>
2025-11-04 14:08:47 +0100

    refactor(structure): create base hexagonal architecture skeleton

---
+++ src/main/java/com/lanny/spring_security_template/web/auth/AuthController.java
@@ -0,0 +1,8 @@
+package com.lanny.spring_security_template.web.auth;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    // Endpoints /auth/login, /auth/refresh, /auth/me
+}
