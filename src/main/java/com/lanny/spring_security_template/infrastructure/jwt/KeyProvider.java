0325824f76aa6aefd93c6fa16a1ddeae532bee5a
lannyrivero <lannyrivero288@gmail.com>
2025-11-04 16:38:06 +0100

    refactor(KeyProvider): change return types to RSAPrivateKey and RSAPublicKey for clarity

--- src/main/java/com/lanny/spring_security_template/infrastructure/jwt/KeyProvider.java
+++ src/main/java/com/lanny/spring_security_template/infrastructure/jwt/KeyProvider.java
@@ -1,9 +1,10 @@
 package com.lanny.spring_security_template.infrastructure.jwt;
 
-import java.security.Key;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 public interface KeyProvider {
-    Key getPrivateKey();
+    RSAPrivateKey getPrivateKey();
 
-    Key getPublicKey();
+    RSAPublicKey getPublicKey();
 }
