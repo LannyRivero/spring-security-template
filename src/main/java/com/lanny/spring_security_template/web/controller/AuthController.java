b746034baf2ce1f8e1cbb4fff947c43c088d012b
lannyrivero <lannyrivero288@gmail.com>
2025-11-04 14:37:43 +0100

    feat(AuthController): add endpoints for login, refresh, and user info retrieval

--- src/main/java/com/lanny/spring_security_template/web/controller/AuthController.java
+++ src/main/java/com/lanny/spring_security_template/web/controller/AuthController.java
@@ -2,7 +2,33 @@ package com.lanny.spring_security_template.web.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lanny.spring_security_template.application.auth.port.in.AuthUseCase;
+import com.lanny.spring_security_template.web.dto.AuthRequest;
+import com.lanny.spring_security_template.web.dto.AuthResponse;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequestMapping("/api/v1/auth")
 public class AuthController {
-    // Endpoints /auth/login, /auth/refresh, /auth/me
+
+    private final AuthUseCase authUseCase;
+
+    public AuthController(AuthUseCase authUseCase) {
+        this.authUseCase = authUseCase;
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        return authUseCase.login(request);
+    }
+
+    @PostMapping("/refresh")
+    public AuthResponse refresh(@RequestParam String refreshToken) {
+        return authUseCase.refresh(refreshToken);
+    }
+
+    @GetMapping("/me")
+    public AuthResponse me(@RequestParam String username) {
+        return authUseCase.me(username);
+    }
 }
