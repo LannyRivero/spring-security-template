01317a7626d9e81f84227820dea79aa51a57189a
lannyrivero <lannyrivero288@gmail.com>
2025-11-05 14:36:38 +0100

    feat(security): add AuthenticationManager bean to SecurityConfig for improved authentication handling

--- src/main/java/com/lanny/spring_security_template/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/lanny/spring_security_template/infrastructure/security/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.lanny.spring_security_template.infrastructure.security.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,6 +46,11 @@ public class SecurityConfig {
                 this.loginRateLimitingFilter = loginRateLimitingFilter;
                 this.securityHeadersFilter = securityHeadersFilter;
                 this.authNoCacheFilter = authNoCacheFilter;
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+                return config.getAuthenticationManager();
         }
 
         @Bean
