4ff1b4e4bf9aef26a6a91622aa2b25560457524b
lannyrivero <lannyrivero288@gmail.com>
2025-11-05 18:02:26 +0100

    refactor(auth): update ScopePolicy interface to rename method and improve clarity in scope resolution

--- src/main/java/com/lanny/spring_security_template/application/auth/port/out/ScopePolicy.java
+++ src/main/java/com/lanny/spring_security_template/application/auth/port/out/ScopePolicy.java
@@ -3,7 +3,5 @@ package com.lanny.spring_security_template.application.auth.port.out;
 import java.util.List;
 
 public interface ScopePolicy {
-    List<String> defaultScopesFor(List<String> roles);
-
-    boolean isAllowed(String scope); // extensible
+    List<String> resolveScopes(List<String> roles);
 }
