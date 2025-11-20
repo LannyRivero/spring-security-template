dd8207d56a1448df8ae0890cf216978d4004ab02
lannyrivero <lannyrivero288@gmail.com>
2025-11-20 14:06:23 +0100

    Implement SessionManager and TokenIssuer classes for session and token management

---
+++ src/main/java/com/lanny/spring_security_template/application/auth/service/TokenIssuer.java
@@ -0,0 +1,52 @@
+package com.lanny.spring_security_template.application.auth.service;
+
+import com.lanny.spring_security_template.application.auth.port.out.TokenProvider;
+import com.lanny.spring_security_template.infrastructure.config.SecurityJwtProperties;
+import com.lanny.spring_security_template.shared.ClockProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
+public class TokenIssuer {
+
+    private final TokenProvider tokenProvider;
+    private final ClockProvider clockProvider;
+    private final SecurityJwtProperties props;
+
+    public IssuedTokens issueTokens(String username, RoleScopeResult rs) {
+
+        Instant now = clockProvider.now();
+        Duration accessTtl = props.accessTtl();
+        Duration refreshTtl = props.refreshTtl();
+
+        Instant accessExp = now.plus(accessTtl);
+        Instant refreshExp = now.plus(refreshTtl);
+
+        String accessToken = tokenProvider.generateAccessToken(
+                username,
+                rs.roleNames(),
+                rs.scopeNames(),
+                accessTtl);
+
+        String refreshToken = tokenProvider.generateRefreshToken(
+                username,
+                refreshTtl);
+
+        String refreshJti = tokenProvider.extractJti(refreshToken);
+
+        return new IssuedTokens(
+                username,
+                accessToken,
+                refreshToken,
+                refreshJti,
+                now,
+                accessExp,
+                refreshExp,
+                rs.roleNames(),
+                rs.scopeNames());
+    }
+}
