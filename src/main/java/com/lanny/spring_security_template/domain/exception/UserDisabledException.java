17a924b48ea7467e4ad7d7187fca561b78044d07
lannyrivero <lannyrivero288@gmail.com>
2025-11-24 11:30:41 +0100

    Refactor UserDisabledException class: enhance documentation for clarity

--- src/main/java/com/lanny/spring_security_template/domain/exception/UserDisabledException.java
+++ src/main/java/com/lanny/spring_security_template/domain/exception/UserDisabledException.java
@@ -1,6 +1,14 @@
 package com.lanny.spring_security_template.domain.exception;
 
+/**
+ * Thrown when a user account has been disabled by an administrator.
+ */
 public class UserDisabledException extends RuntimeException {
+
+    public UserDisabledException() {
+        super("User account is disabled");
+    }
+
     public UserDisabledException(String message) {
         super(message);
     }
