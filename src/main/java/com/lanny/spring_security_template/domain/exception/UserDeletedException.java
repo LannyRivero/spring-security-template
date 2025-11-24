b70c72e70d492a326175149ed9a4f3c5f6abee0e
lannyrivero <lannyrivero288@gmail.com>
2025-11-24 11:31:20 +0100

    Refactor UserDeletedException class: enhance documentation for clarity

--- src/main/java/com/lanny/spring_security_template/domain/exception/UserDeletedException.java
+++ src/main/java/com/lanny/spring_security_template/domain/exception/UserDeletedException.java
@@ -1,6 +1,15 @@
 package com.lanny.spring_security_template.domain.exception;
 
+/**
+ * Thrown when a user account has been logically deleted (soft delete)
+ * and is no longer allowed to authenticate.
+ */
 public class UserDeletedException extends RuntimeException {
+
+  public UserDeletedException() {
+    super("User account has been deleted");
+  }
+
   public UserDeletedException(String message) {
     super(message);
   }
