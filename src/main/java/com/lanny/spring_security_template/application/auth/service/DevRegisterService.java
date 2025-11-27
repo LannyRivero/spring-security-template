99228a9f006222f8792597a4ec4f28a3084c12f9
lannyrivero <lannyrivero288@gmail.com>
2025-11-27 13:25:29 +0100

    Implement password policy validation and default implementation for user registration

--- src/main/java/com/lanny/spring_security_template/application/auth/service/DevRegisterService.java
+++ src/main/java/com/lanny/spring_security_template/application/auth/service/DevRegisterService.java
@@ -1,13 +1,14 @@
 package com.lanny.spring_security_template.application.auth.service;
 
 import com.lanny.spring_security_template.application.auth.command.RegisterCommand;
+import com.lanny.spring_security_template.application.auth.policy.PasswordPolicy;
+import com.lanny.spring_security_template.application.auth.port.out.AuthMetricsService;
 import com.lanny.spring_security_template.application.auth.port.out.UserAccountGateway;
 import com.lanny.spring_security_template.domain.model.User;
 import com.lanny.spring_security_template.domain.service.PasswordHasher;
 import com.lanny.spring_security_template.domain.valueobject.EmailAddress;
 import com.lanny.spring_security_template.domain.valueobject.PasswordHash;
 import com.lanny.spring_security_template.domain.valueobject.Username;
-import com.lanny.spring_security_template.infrastructure.metrics.AuthMetricsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +18,12 @@ public class DevRegisterService {
 
     private final UserAccountGateway userAccountGateway;
     private final PasswordHasher passwordHasher;
-    private final AuthMetricsServiceImpl metrics;
+    private final AuthMetricsService metrics;
+    private final PasswordPolicy passwordPolicy;
 
     public void register(RegisterCommand cmd) {
+
+        passwordPolicy.validate(cmd.rawPassword());
 
         User newUser = User.createNew(
                 Username.of(cmd.username()),
