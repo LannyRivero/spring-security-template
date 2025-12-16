dd9ef05425b5514d9b1854a8c63a6712eb45ce61
lannyrivero <lannyrivero288@gmail.com>
2025-12-16 11:50:28 +0100

    feat(security): isolate login transaction with requires new propagation

--- src/main/java/com/lanny/spring_security_template/infrastructure/adapter/usecase/transactional/LoginTransactionalAdapter.java
+++ src/main/java/com/lanny/spring_security_template/infrastructure/adapter/usecase/transactional/LoginTransactionalAdapter.java
@@ -1,6 +1,7 @@
 package com.lanny.spring_security_template.infrastructure.adapter.usecase.transactional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lanny.spring_security_template.application.auth.command.LoginCommand;
@@ -8,6 +9,14 @@ import com.lanny.spring_security_template.application.auth.result.JwtResult;
 import com.lanny.spring_security_template.application.auth.service.LoginService;
 
 import lombok.RequiredArgsConstructor;
+/**
+ * Transactional adapter fot login use case
+ * 
+ * -Independent transaction boundary
+ * Atomic istence of refresh token and session state
+ * -No propagation of external transactions
+ * 
+ */
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +24,7 @@ public class LoginTransactionalAdapter {
 
     private final LoginService loginService;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public JwtResult login(LoginCommand cmd) {
         return loginService.login(cmd);
     }
